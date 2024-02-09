514b66e13f38e0d94017bb1f4ae51598d99a2f09
AngelZ1305 <133991607+AngelZ1305@users.noreply.github.com>
2024-02-09 15:50:28 -0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin-bottom: 20px;
+    }
+
+    th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    th {
+      background-color: #f2f2f2;
+    }
+  </style>
+<body>
+    <table>
+    <tr>
+      <th>Materias</th>
+      <th>Cursos</th>
+      <th>Kardex</th>
+      <th>Calificaciones</th>
+      <th>Idiomas</th>
+      <th>Adeudos</th>
+      <th><a href="index.php">Salir</a> </th>
+    </tr>
+  </table>
+  <p>Pongame un 100 profe :D</p>
+
+    
+</body>
+</html>
